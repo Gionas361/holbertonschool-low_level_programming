fcc8e8fc06313f30edfefda7560536c7ab8e8147
Gionas361 <giovannicarmona57@gmail.com>
2023-06-15 15:48:10 +0000

    4c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(void) /*It function that is run at the start when the file is ran*/
+int main(void)
 {
+/*puts will print the text inside of the quotation*/
 puts("\"Programming is like building a multilingual puzzle");
-
+/*return will wake the value of void = 0*/
 return (0);
 }
