31aad64f79b38a40bd779553ce7eabf0e444a9d1
Gionas361 <giovannicarmona57@gmail.com>
2023-06-15 17:53:57 +0000

    6ccccssss

---
+++ hello_world/6-size.c
@@ -0,0 +1,20 @@
+/*
+ * File: 6-size.c
+ * Auth: Giovanni Y. Carmona <6838@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+/**
+ * main - idk what im DOING!!!!!
+ * Return: will wake the value of void = 0
+ */
+int main(void)
+{
+printf(sizeof(char));
+printf(sizeof(int));
+printf(sizeof(long int));
+printf(sizeof(long long int));
+printf(sizeof(float));
+
+return (0);
+}
