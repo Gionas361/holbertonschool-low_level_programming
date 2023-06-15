adf039793a54227c6e0103251000b765b31ec153
Gionas361 <giovannicarmona57@gmail.com>
2023-06-15 17:26:19 +0000

    5f

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File: 5-printf.c
+ * Auth: Giovanni Y. Carmona <6838@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+/**
+ * main - printf will print the text inside of the quotation
+ * Return: will wake the value of void = 0
+ */
+int main(void)
+{
+
+printf("with proper grammar, but the outcome is a piece of art,");
+
+return (0);
+}
