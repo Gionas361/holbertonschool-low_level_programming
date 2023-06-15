bbf58de04d8fb7f0380f1430b07cfa03067a7052
Gionas361 <giovannicarmona57@gmail.com>
2023-06-15 15:49:54 +0000

    4c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-
+/*main - puts will print the text inside of the quotation*/
+/*return - will wake the value of void = 0*/
 int main(void)
 {
-/*puts will print the text inside of the quotation*/
+
 puts("\"Programming is like building a multilingual puzzle");
-/*return will wake the value of void = 0*/
+
 return (0);
 }
