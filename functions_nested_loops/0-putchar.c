bcab45252c204d3721923d9378defd6b76425908
Gionas361 <giovannicarmona57@gmail.com>
2023-06-19 17:36:58 +0000

    putchar print

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Giovanni Y. Carmona <6838@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+/**
+ * main - puts will print the text inside of the quotation
+ * Return: will wake the value of void = 0
+ */
+int main(void)
+{
+
+puts("_putchar");
+
+return (0);
+}
