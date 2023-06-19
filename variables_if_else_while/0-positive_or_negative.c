8cddccffaf91c53e471ea0c68d6b236fd309c381
Gionas361 <giovannicarmona57@gmail.com>
2023-06-19 13:50:04 +0000

    1st try?

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+/*
+ * File: 0-positive_or_negative.c
+ * Auth: Giovanni Y. Carmona <6838@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+/**
+ * main - idk what im DOING!!!!!
+ * Return: will wake the value of void = 0
+ */
+int main(void)
+{
+    if (.n > 0)
+    {
+        printf(.n, " is negative");
+    }
+    else (.n < 0)
+    {
+        printf(.n, "is positive");
+    }
+
+    return(0);
+}
