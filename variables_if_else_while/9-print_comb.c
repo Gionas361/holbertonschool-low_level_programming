52fd15a69b36e2709688aa128ea1c959730f0f3a
Gionas361 <giovannicarmona57@gmail.com>
2023-06-19 15:39:27 +0000

    test 1

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Giovanni Y. Carmona <6838@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+/**
+ * main - idk what im DOING!!!!!
+ * Return: will wake the value of void = 0
+ */
+int main(void)
+{
+char ch;
+char separar = ', ';
+char n = '\n';
+
+for (ch = '0' ; ch <= '99' ; ch++)
+{
+putchar(ch);
+putchar(separar);
+}
+putchar(n);
+
+return (0);
+}
