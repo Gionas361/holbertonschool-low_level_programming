c740ce6c87b6fa6e7480c2273e07bda1b762cd6d
Gionas361 <giovannicarmona57@gmail.com>
2023-06-19 15:35:38 +0000

    ez pog?

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: Giovanni Y. Carmona <6838@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+/**
+ * main - idk what im DOING!!!!!
+ * Return: will wake the value of void = 0
+ */
+int main(void)
+{
+char ch;
+char n = '\n';
+
+for (ch = 'z' ; ch >= 'a' ; ch--)
+{
+putchar(ch);
+}
+putchar(n);
+
+return (0);
+}
