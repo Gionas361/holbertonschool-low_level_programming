c353111d3e2b342f1bba632dd95749d2a97970fc
Gionas361 <giovannicarmona57@gmail.com>
2023-06-19 14:50:09 +0000

    put me on a jar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Giovanni Y. Carmona <6838@holbertonstudents.com>
+ */
+
+#include <stdio.h>
+/**
+ * main - idk what im DOING!!!!!
+ * Return: will wake the value of void = 0
+ */
+int main(void)
+{
+putchar([[a-z]])
+putchar("\n")
+
+return (0);
+}
