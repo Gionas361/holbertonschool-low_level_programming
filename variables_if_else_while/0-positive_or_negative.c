2860a33aa91b269dfd299826802dad9074365b3b
Gionas361 <giovannicarmona57@gmail.com>
2023-06-19 14:00:11 +0000

    08435398031th

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,11 @@
  * File: 0-positive_or_negative.c
  * Auth: Giovanni Y. Carmona <6838@holbertonstudents.com>
  */
-
+ 
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - idk what im DOING!!!!!
  * Return: will wake the value of void = 0's
