5af51cc68c883e13fd3eddabca813bdd5f9db2a9
Gionas361 <giovannicarmona57@gmail.com>
2023-06-19 13:59:18 +0000

    25ths

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -3,13 +3,20 @@
  * Auth: Giovanni Y. Carmona <6838@holbertonstudents.com>
  */
 
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - idk what im DOING!!!!!
  * Return: will wake the value of void = 0's
  */
 int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+/* This is my code */
     if (n > 0)
     {
         printf(n, " is negative");
