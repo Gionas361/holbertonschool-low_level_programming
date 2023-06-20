f743a99c2e1408e40d088cb0e1c98d0ca4afb34e
Gionas361 <giovannicarmona57@gmail.com>
2023-06-20 17:40:49 +0000

    there

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,10 +5,9 @@
  * _islower - prints 1 of the string provided is a lower case letter
  *            and prints 0 if the string is anything else.
  *
- * 'int' - is the string provided by the user
- * 'c' - is the result of the calculation
+ * @param c - is the string provided by the user
  *
- * Return: Always 0.
+ * Return: gives out the result of calculation.
  */
 int _islower(int c)
 {
