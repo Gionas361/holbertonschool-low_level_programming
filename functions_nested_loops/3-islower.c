f8ddb2f49162a7547aaa6109f4e33e386cf3c20a
Gionas361 <giovannicarmona57@gmail.com>
2023-06-20 15:09:35 +0000

    betty u fucking cunt

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,6 +5,7 @@
  * _islower - prints 1 of the string provided is a lower case letter
  *            and prints 0 if the string is anything else.
  *
+ * int - is the string provided by the user
  * c - is the result of the calculation
  *
  * Return: Always 0.
@@ -13,10 +14,10 @@ int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
-		return(1);
+		return (1);
 	}
 	else
 	{
-		return(0);
+		return (0);
 	}
 }
