056bdb23b387c52383a69507f67a23001d322279
Gionas361 <giovannicarmona57@gmail.com>
2023-06-20 13:36:04 +0000

    wtf

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,17 +7,6 @@
  * Return: Always 0.
  */
 int print_alphabet(void)
-{
-	char ch;
-
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-
-	return (0);
-}
 
 int main(void)
 {
