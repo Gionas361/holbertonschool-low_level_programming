4529ec0310c5f79b66ff3c70f936785a8d62ccd4
Gionas361 <giovannicarmona57@gmail.com>
2023-06-20 18:41:45 +0000

    oneshot pog?

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - turns a negative number, into a positive one
+ *
+ * @n: The string provided by the user
+ *
+ * Return: gives out the result of calculation.
+ */
+int _abs(int n)
+{
+	char f;
+
+	if (n < 0)
+	{
+		f = (n * -1);
+	}
+    else if (n >= 0)
+    {
+        f = n;
+    }
+	return (f);
+	_putchar('\n');
+}
