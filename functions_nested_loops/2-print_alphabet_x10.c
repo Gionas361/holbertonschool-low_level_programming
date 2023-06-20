de3fbc35b8c80a9003b9a7c290078f7209bc6b99
Gionas361 <giovannicarmona57@gmail.com>
2023-06-20 14:30:39 +0000

    10 alphabets

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int print_alphabet(void)
+{
+    char nTimes;
+
+    for (nTimes = '0' ; nTimes <= '10' ; ch++)
+    {
+	    char ch;
+
+    	for (ch = 'a' ; ch <= 'z' ; ch++)
+	    {
+	    	_putchar(ch);
+	    }
+	    _putchar('\n');
+    }
+
+	return (0);
+}
+
+
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
