2cfe9f23bcf79ffa8a815f3bbce9a7b4c6e9f6ca
Gionas361 <giovannicarmona57@gmail.com>
2023-06-20 15:00:32 +0000

    problem 3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _islower - prints 1 of the string provided is a lower case letter
+ *            and prints 0 if the string is anything else.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (c >= a && c <= z)
+    {
+        _putchar('1')
+    }
+    else
+    {
+        _putchar('0')
+    }
+
+	return (0);
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -12,3 +12,5 @@ int _putchar(char c);
 int print_alphabet();
 
 int print_alphabet_x10();
+
+int _islower(int c);
