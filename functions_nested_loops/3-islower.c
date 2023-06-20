6bc48b8a8cded5828a2fa8206eb259fc39dc356c
Gionas361 <giovannicarmona57@gmail.com>
2023-06-20 17:36:26 +0000

    maybe

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -5,8 +5,8 @@
  * _islower - prints 1 of the string provided is a lower case letter
  *            and prints 0 if the string is anything else.
  *
- * int - is the string provided by the user
- * c - is the result of the calculation
+ * 'int' - is the string provided by the user
+ * 'c' - is the result of the calculation
  *
  * Return: Always 0.
  */
