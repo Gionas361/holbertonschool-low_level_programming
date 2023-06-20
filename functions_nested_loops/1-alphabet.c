0afdd7a9e0d077f410af711fc6081392ab4f53fa
Gionas361 <giovannicarmona57@gmail.com>
2023-06-20 13:53:16 +0000

    fix?

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -7,8 +7,15 @@
  * Return: Always 0.
  */
 
-int main(void)
+int print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+
+	return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -11,16 +11,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-int print_alphabet(void)
-{
-	char ch;
-
-	for (ch = 'a' ; ch <= 'z' ; ch++)
-	{
-		_putchar(ch);
-	}
-	_putchar('\n');
-
-	return (0);
-}
