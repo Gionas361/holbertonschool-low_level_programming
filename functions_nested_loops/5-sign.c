7ed4ab1ca44a0756ce91b614b9be844832074ca8
Gionas361 <giovannicarmona57@gmail.com>
2023-06-20 18:24:31 +0000

    f

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -11,27 +11,29 @@
  */
 int print_sign(int n)
 {
+    char f;
+
 	if (n > 0)
 	{
 		_putchar('+');
 		_putchar(',');
 		_putchar(' ');
-		return (1);
+		f = 1;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
 		_putchar(',');
 		_putchar(' ');
-		return (-1);
+		f = -1;
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
 		_putchar(',');
 		_putchar(' ');
-		return (0);
+		f = 0;
 	}
+	return (f);
 	_putchar('\n');
-
 }
