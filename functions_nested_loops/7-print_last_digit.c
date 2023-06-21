e59c52d46ca9e42bc515ce79d1a37ae68fa70946
Gionas361 <giovannicarmona57@gmail.com>
2023-06-21 12:46:28 +0000

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_last_digit - prints out the last digit of the number string provided
+ *
+ * @n: The string provided by the user
+ *
+ * Return: gives out the result of calculation.
+ */
+
+int print_sign(int n)
+{
+	char f;
+
+	f = n % 10
+	return (f);
+	_putchar('\n');
+}
