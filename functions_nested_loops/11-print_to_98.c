c13ee53bb1fc89e7fec84a2722d9e43eae4c4580
Gionas361 <giovannicarmona57@gmail.com>
2023-06-21 17:25:02 +0000

    comd

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,38 +11,38 @@
 
 int print_to_98(int n)
 {
-    int vnum;
+	int vnum;
 
-    if (n < 98)
-    {
-	    for (vnum = n ; n == 98 ; n++)
-	    {
-            vnum = n;
-            if (vnum < 98)
-            {
-	            printf("%d, ", vnum);
-            }
-            else if (vnum == 98)
-            {
-                printf("%d", vnum);
-            }
-        }
-    }
-    if (n > 98)
-    {
-	    for (vnum = n ; n == 98 ; n--)
-    	{
-            vnum = n;
-            if (vnum > 98)
-            {
-    		    printf("%d, ", vnum);
-            }
-            else if (vnum == 98)
-            {
-                printf("%d", vnum);
-            }
-    	}
-    }
+	if (n < 98)
+	{
+		for (vnum = n ; n == 98 ; n++)
+		{
+			vnum = n;
+			if (vnum < 98)
+			{
+				printf("%d, ", vnum);
+			}
+			else if (vnum == 98)
+			{
+				printf("%d", vnum);
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (vnum = n ; n == 98 ; n--)
+		{
+			vnum = n;
+			if (vnum > 98)
+			{
+				printf("%d, ", vnum);
+			}
+			else if (vnum == 98)
+			{
+				printf("%d", vnum);
+			}
+		}
+	}
 
 	return (0);
 }
