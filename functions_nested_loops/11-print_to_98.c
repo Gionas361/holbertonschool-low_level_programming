eafcb17bf5f16e7ea8c79f90d60b987dd1aaf2a7
Gionas361 <giovannicarmona57@gmail.com>
2023-06-21 17:30:50 +0000

    cum

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,16 +8,6 @@
  *
  * Return: gives out the result of calculation.
  */
-int main(void)
-{
-    print_to_98(0);
-    print_to_98(98);
-    print_to_98(111);
-    print_to_98(81);
-    print_to_98(-10);
-    return (0);
-}
-
 int print_to_98(int n)
 {
 	if (n <= 98)
