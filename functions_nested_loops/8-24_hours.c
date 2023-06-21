45a83fdfd9cc94497c21f163d49196e536769858
Gionas361 <giovannicarmona57@gmail.com>
2023-06-21 15:16:38 +0000

    comment

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 
 /**
- * print_last_digit - prints out the last digit of the number string provided
+ * jack_bauer - prints out the last digit of the number string provided
  *
- * @n: The string provided by the user
  *
  * Return: gives out the result of calculation.
  */
