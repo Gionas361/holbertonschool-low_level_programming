42a9d1a8bc371c1b733f7f3e89fc639422c42984
Gionas361 <giovannicarmona57@gmail.com>
2023-06-22 15:12:00 +0000

    F#CK BETTY

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,44 +2,42 @@
 #include <stdio.h>
 
 /**
- * print_square - prints a square with #.
- *
- * @size: the string provided by the user
+ * main - prints a fizzbuzz.
  *
  * Return: gives out the result of calculation.
  */
 
 int main(void)
 {
-    int fi = 3;
-    int bu = 5;
-    int num;
+	int fi = 3;
+	int bu = 5;
+	int num;
 
-    for (num = 1 ; num <= 100 ; num++)
-    {
-        if ((num % fi) == 0 && ((num % bu) != 0))
-        {
-            printf("Fizz");
-        }
-        else if ((num % bu) == 0 && ((num % fi) != 0))
-        {
-            printf("Buzz");
-        }
-        else if (((num % bu) == 0) && ((num % bu) == 0))
-        {
-            printf("FizzBuzz");
-        }
-        else
-        {
-            printf("%d", num);
-        }
+	for (num = 1 ; num <= 100 ; num++)
+	{
+		if ((num % fi) == 0 && ((num % bu) != 0))
+		{
+			printf("Fizz");
+		}
+		else if ((num % bu) == 0 && ((num % fi) != 0))
+		{
+			printf("Buzz");
+		}
+		else if (((num % bu) == 0) && ((num % bu) == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else
+		{
+			printf("%d", num);
+		}
 
-        if (num != 100)
-        {
-            printf(" ");
-        }
-    }
-    printf("\n");
+		if (num != 100)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
 
 	return (0);
 }
