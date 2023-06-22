f22a28a0c06c0df1c42e6369277d247a2ae67e05
Gionas361 <giovannicarmona57@gmail.com>
2023-06-22 13:57:07 +0000

    com

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_most_numbers - prints the numbers from 0 to 9, except 2 and 4
+ * more_numbers - prints the numbers from 0 to 14
  *
  * Return: gives out the result of calculation.
  */
