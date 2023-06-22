20bd4bb34c47e871f23b1b8b51d38a148a952669
Gionas361 <giovannicarmona57@gmail.com>
2023-06-22 14:39:09 +0000

    it goes in the square hole

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_square - prints a square with #.
+ *
+ * @size: the string provided by the user
+ *
+ * Return: gives out the result of calculation.
+ */
+
+int print_square(int size)
+{
+	int lenght;
+	int height;
+
+	for (height = 0 ; height < n ; height++)
+	{
+		for (lenght = 0 ; lenght < n ; lenght--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -24,3 +24,5 @@ int more_numbers(void);
 int print_line(int n);
 
 int print_diagonal(int n);
+
+int print_square(int size);
