b70ee902709098481f7f9193acfb54801d39b7b6
Gionas361 <giovannicarmona57@gmail.com>
2023-06-22 13:45:42 +0000

    most of em

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints the numbers from 0 to 9, except 2 and 4
+ *
+ * Return: gives out the result of calculation.
+ */
+
+int print_most_numbers(void)
+{
+	int r;
+
+	for (r = 0 ; r <= 9 ; r++)
+	{
+		if (r != 2 || 4)
+		{
+		printf("%d", r);
+		}
+	}
+	printf("\n");
+
+	return (0);
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -16,3 +16,5 @@ int _isdigit(int c);
 int mul(int a, int b);
 
 int print_numbers(void);
+
+int print_most_numbers(void);
