4090becef8fb5f97f3068098a811517e4071fb50
Gionas361 <giovannicarmona57@gmail.com>
2023-06-26 18:14:26 +0000

    +1

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -9,18 +9,6 @@
  *
  * Return: gives out the result of calculation.
  */
-int main(void)
-{
-    int array[5];
-
-    array[0] = 98;
-    array[1] = 402;
-    array[2] = -198;
-    array[3] = 298;
-    array[4] = -1024;
-    print_array(array, 5);
-    return (0);
-}
 
 int print_array(int *a, int n)
 {
@@ -33,10 +21,10 @@ int print_array(int *a, int n)
 	for (currstr = 0 ; currstr < countnum ; currstr++)
 	{
 		printf("%d", a[currstr]);
-        if (currstr +1 < countnum)
-        {
-            printf(", ");
-        }
+		if (currstr +1 < countnum)
+		{
+			printf(", ");
+		}
 	}
 	printf("\n");
 
