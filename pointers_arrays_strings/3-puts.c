0da491f45d6af19111ec2ce783779046c12670fb
Gionas361 <giovannicarmona57@gmail.com>
2023-06-26 14:32:38 +0000

    done with an array

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints a triangle
+ *
+ * @str: the string provided by the user
+ *
+ * Return: gives out the result of calculation.
+ */
+
+int _puts(char *str)
+{
+	int currstr;
+	int countnum;
+
+	for (countnum = 0; str[countnum] ; countnum++);
+
+	for (currstr = 0 ; currstr <= countnum ; currstr++)
+	{
+		printf("%c", str[currstr]);
+	}
+	printf("\n");
+
+	return (0);
+}
