40a486c3026b279470a25dd47569c402af9416d4
Gionas361 <giovannicarmona57@gmail.com>
2023-06-26 18:24:02 +0000

    9th

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - prints a triangle
+ *
+ * @s: the string provided by the user
+ * @s: the string provided by the user
+ *
+ * Return: gives out the result of calculation.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (0);
+}
