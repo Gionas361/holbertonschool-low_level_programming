578384643f76189683c8f3bfd4f27121a0588447
Gionas361 <giovannicarmona57@gmail.com>
2023-06-26 13:47:58 +0000

    first try

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - prints a triangle
+ *
+ * @a: the string provided by the user
+ * @b: the string provided by the user
+ *
+ * Return: gives out the result of calculation.
+ */
+
+int swap_int(int *a, int *b)
+{
+	int swap1 = *a;
+	int swap2 = *b;
+
+	*a = swap2;
+    *b = swap1;
+
+	return (0);
+}
