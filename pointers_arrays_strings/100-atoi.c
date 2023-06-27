df552a2ba7c43c9b0ac27678952bef8f2d8b7dc1
Gionas361 <giovannicarmona57@gmail.com>
2023-06-27 18:58:38 +0000

    save

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _atoi - prints a triangle
+ *
+ * @s: the string provided by the user
+ *
+ * Return: gives out the result of calculation.
+ */
+
+int _atoi(char *s)
+{
+    int stringLenght;
+	int numCheck;
+
+    for (i = 0 ; s[i] ; i++)
+    {
+    }
+
+    for ()
+
+	return (int baby);
+}
