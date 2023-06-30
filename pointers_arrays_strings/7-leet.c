4a8c929196fe6bb2d74164f930cbc0a1bf11cebd
Gionas361 <giovannicarmona57@gmail.com>
2023-06-30 15:20:07 +0000

    mozart

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *leet(char *s)
+{
+	char letters[] = {'A', 'E', 'O', 'T', 'L'};
+	char numstrs[] = {'4', '3', '0', '7', '1'};
+	int currltr;
+	int currstr;
+
+	for (currstr = 0 ; s[currstr] != '\0' ; currstr++)
+	{
+		for (currltr = 0 ; currltr < 4 ; currltr++)
+		{
+	    	if (s[currstr] == letters[currltr] || s[currstr] - 32 == letters[currltr])
+		    {
+		    	s[currstr] = numstrs[currltr];
+		    }
+		}
+	}
+
+	return (s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -45,3 +45,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *i);
 
 char *cap_string(char *i);
+
+char *leet(char *s);
