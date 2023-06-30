110c8806045fab7c445af6ca808c3d44e08a4503
Gionas361 <giovannicarmona57@gmail.com>
2023-06-30 17:26:18 +0000

    changes

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - prints a triangle
+ *
+ * @s: the string provided by the user
+ * @c: the string provided by the user
+ *
+ * Return: gives out the result of calculation.
+ */
+
+char *_strchr(char *s, char c)
+{
+	int currstr;
+    char *finaleval;
+
+	for (currstr = 0 ; s[currstr] != c ; currstr++)
+    {
+        finaleval = &s[currstr + 1];
+    }
+
+	return (finaleval);
+}
