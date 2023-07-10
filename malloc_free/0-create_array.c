98dd655cafec8e3cb6204464e64a14eaa1d29a10
Gionas361 <giovannicarmona57@gmail.com>
2023-07-10 10:38:09 -0400

    test 1

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - makes an array
+ * 
+ * @size: size of array
+ * @c: idk
+ * 
+ * Return: Retuns 0
+*/
+
+char *create_array(unsigned int size, char c)
+{
+    char *ctoo;
+    unsigned int s;
+
+    ctoo = malloc(size * sizeof(c));
+    for (s = 0 ; s != size ; s++)
+    {
+        ctoo[s] = c;
+    }
+
+    if (size == 0)
+    {
+        return (NULL);
+    }
+    else if (ctoo == NULL)
+    {
+        return (NULL);
+    }
+
+    return(ctoo);
+}
