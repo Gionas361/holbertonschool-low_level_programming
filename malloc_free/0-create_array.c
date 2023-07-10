c09f9e87d01e0605120dc972fafc0504b0225cb7
Gionas361 <giovannicarmona57@gmail.com>
2023-07-10 10:43:38 -0400

    betty tab

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -13,23 +13,23 @@
 
 char *create_array(unsigned int size, char c)
 {
-    char *ctoo;
-    unsigned int s;
+	char *ctoo;
+	unsigned int s;
 
-    ctoo = malloc(size * sizeof(c));
-    for (s = 0 ; s != size ; s++)
-    {
-        ctoo[s] = c;
-    }
+	ctoo = malloc(size * sizeof(c));
+	for (s = 0 ; s != size ; s++)
+	{
+		ctoo[s] = c;
+	}
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
-    else if (ctoo == NULL)
-    {
-        return (NULL);
-    }
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	else if (ctoo == NULL)
+	{
+		return (NULL);
+	}
 
-    return(ctoo);
+	return(ctoo);
 }
