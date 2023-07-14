90e01e265bc731b1970d63cce19cfaef0838428a
Gionas361 <giovannicarmona57@gmail.com>
2023-07-14 14:16:15 -0400

    tesy

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,22 +13,40 @@
 
 int **alloc_grid(int width, int height)
 {
-    int **griid;
+	int **griid;
 
-    griid = malloc(sizeof(int *) * height);
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
-    }
+	griid = malloc(sizeof(int *) * height);
+	if (griid == NULL)
+	{
+		return (NULL);
+	}
 
-    for (; height != '\0' ; height++)
-    {
-        for (; width != '\0' ; width++)
-        {
-            griid[height][width] = 0;
-        }
-    }
+	for (height = 0 ; height != '\0'; height++)
+	{
+		griid[height] = malloc(sizeof(int) * width);
+		if (griid[height] == NULL)
+		{
+            for (width = 0 ; width != '\0' ; width++)
+            {
+			    free(griid[width]);
+            }
+		}
+		free(griid);
+		return (NULL);
+	}
 
-    return (griid);
+	for (height = 0 ; height != '\0' ; height++)
+	{
+		for (width = 0 ; width != '\0' ; width++)
+		{
+			griid[height][width] = 0;
+		}
+	}
+
+	return (griid);
 }
