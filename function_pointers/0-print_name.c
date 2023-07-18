263261aee271a245551c30c176080d2676487d89
Gionas361 <giovannicarmona57@gmail.com>
2023-07-18 14:07:27 -0400

    comment fix?

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
- * print_name - references the name given to the function received
- * @name: the name given
- * @f: pointer to the function received
- *
- * Return: Nothing.
- */
+  * print_name - references the name given to the function received.
+  * @name: the name given.
+  * @f: pointer to the function received.
+  *
+  * Return: Nothing.
+  */
 
 void print_name(char *name, void (*f)(char *))
 {
