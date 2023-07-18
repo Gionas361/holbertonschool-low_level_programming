183bfd514921bb9b7ec48fc289ad4144a253365d
Gionas361 <giovannicarmona57@gmail.com>
2023-07-18 11:13:18 -0400

    fixes

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,13 +16,12 @@ struct dog
 	char* owner;
 };
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
 /**
  * dog_t - Typedef for unsigned int
  */
 typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
