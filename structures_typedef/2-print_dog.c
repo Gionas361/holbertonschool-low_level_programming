c9c4f64d7cc046c14aaa40b25d6c957b24ffe798
Gionas361 <giovannicarmona57@gmail.com>
2023-07-18 09:51:30 -0400

    test one

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,50 @@
+/**
+  * To PRINT, or not to print. THAT IS THE QUESTION!!!
+  */
+
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+  * init_dog - Initializes a dog structure
+  * @d: dog structure.
+  *
+  * Return: Nothing
+  */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: ");
+		if ((*d).name != NULL)
+		{
+			printf("%s\n", (*d).name);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+
+		printf("Age: ");
+		if ((*d).age != ' ')
+		{
+			printf("%f\n", (*d).age);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+
+		printf("Owner: ");
+		if ((*d).owner != NULL)
+		{
+			printf("%s\n", (*d).owner);
+		}
+		else
+		{
+			printf("(nil)");
+		}
+	}
+}
