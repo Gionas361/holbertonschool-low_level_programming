ac94b896db62ad7e8d6ec4d222bfbafe07d228e1
Gionas361 <giovannicarmona57@gmail.com>
2023-07-18 13:35:35 -0400

    upload?

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -2,15 +2,13 @@
 #include <stdlib.h>
 
 /**
-  * init_dog - Initializes a dog structure
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
+  * free_dog - frees' a dog structure
+  * @d: The name of the dog.
   *
   * Return: Nothing
   */
 
 void free_dog(dog_t *d)
 {
-
+	free(d);
 }
