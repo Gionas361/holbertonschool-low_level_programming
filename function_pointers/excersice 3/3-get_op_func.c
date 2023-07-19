40a4ae581e5b41ce142c1c4fa4ebc78d3b37ebf6
Gionas361 <giovannicarmona57@gmail.com>
2023-07-19 14:29:15 -0400

    finished

---
+++ function_pointers/excersice 3/3-get_op_func.c
@@ -0,0 +1,37 @@
+#include "3-calc.h"
+#include <string.h>
+
+int looper(char sumb, int maxamount);
+
+/**
+  * get_op_func - gets the correct mathematical operation
+  * @s: character used for the calculation that was provided
+  *
+  * Return: result.
+  */
+
+int (*get_op_func(char *s))(int, int)
+{
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
+    int i = 0;
+
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+        {
+			return (ops[i].f);
+        }
+
+		i++;
+	}
+
+	return (0);
+}
---
+++ function_pointers/excersice 3/3-op_functions.c
@@ -0,0 +1,72 @@
+#include "3-calc.h"
+
+/**
+  * op_add - sums
+  * @a: first value
+  * @b: second value
+  *
+  * Return: result.
+  */
+
+int op_add(int a, int b)
+{
+    return (a+b);
+}
+
+/**
+  * op_sub - substraction
+  * @a: first value
+  * @b: second value
+  *
+  * Return: result.
+  */
+
+int op_sub(int a, int b)
+{
+    return (a-b);
+}
+
+/**
+  * op_mul - multiplies
+  * @a: first value
+  * @b: second value
+  *
+  * Return: result.
+  */
+
+int op_mul(int a, int b)
+{
+    return (a*b);
+}
+
+/**
+  * op_div - result of divide
+  * @a: first value
+  * @b: second value
+  *
+  * Return: result.
+  */
+
+int op_div(int a, int b)
+{
+    return (a/b);
+}
+
+/**
+  * op_mod - remainder of divide
+  * @a: first value
+  * @b: second value
+  *
+  * Return: result.
+  */
+
+int op_mod(int a, int b)
+{
+    int i = a/b;
+
+    i = b*i;
+
+    i = a-i;
+
+    return(i);
+}
