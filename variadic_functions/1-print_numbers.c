e3fb4f7b07d9a2ab141b54ae1f863faa8b5ab1ec
Gionas361 <giovannicarmona57@gmail.com>
2023-07-21 09:56:37 -0400

    excersice 2

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,42 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+  * sum_them_all - sums all the numers given.
+  * @n: Amount of numbers provided.
+  * @...: next numbers.
+  *
+  * Return: Nothing.
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int currNum;
+
+	va_start(nums, n);
+
+	for (currNum = 0 ; currNum < n ; currNum++)
+	{
+		printf("%d", va_arg(nums, int));
+        if (currNum == (n - 1) || separator == NULL)
+        {
+            printf("\n");
+        }
+        else
+        {
+            printf("%c", *separator);
+            printf(" ");
+        }
+	}
+
+	va_end(nums);
+}
+
+/**
+  * for (i = 0 ; separator[i] = "\0" ; i++)
+  * {
+  *     printf("@c", separator);
+  * }
+  */
