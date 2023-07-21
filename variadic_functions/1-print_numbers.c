d3742b430f0776f2dfcbb495845d5ab7686e17c7
Gionas361 <giovannicarmona57@gmail.com>
2023-07-21 10:03:31 -0400

    fis

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -29,7 +29,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		{
 			printf("\n");
 		}
-		else
+		else if (separator != NULL || separator != "")
 		{
 			printf("%c", *separator);
 			printf(" ");
