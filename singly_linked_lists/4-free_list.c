a8c4a3486d466faa090fc49c359dc6251a6f0001
Gionas361 <giovannicarmona57@gmail.com>
2023-08-02 10:00:54 -0400

    Cant run valgrind so text on intranet

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * free_list - adds a new node to the top of the list.
+ *
+ * @head: the list given to us.
+ *
+ * Return: nothing.
+ */
+void free_list(list_t *head)
+{
+	list_t *freelist;
+
+	while (head)
+	{
+		freelist = (*head).next;
+		free((*head).str);
+		free(head);
+		head = freelist;
+	}
+}
