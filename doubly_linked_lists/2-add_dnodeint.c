fc68b42319d93b4a008adbf794af1a5dbfaf791d
Gionas361 <giovannicarmona57@gmail.com>
2023-08-02 12:55:56 -0400

    Double trouble

---
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * add_dnodeint - adds a new node to the top of the list.
+ *
+ * @head: the list given to us.
+ * @n: integer of node?
+ *
+ * Return: pointer to node.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *nodepointer;
+
+	nodepointer = malloc(sizeof(dlistint_t));
+
+	if (nodepointer == NULL)
+	{
+		return (NULL);
+	}
+
+	if (n < 0)
+	{
+		free(nodepointer);
+		return (NULL);
+	}
+
+	(*nodepointer).n = n;
+	(*nodepointer).next = *head;
+
+	*head = nodepointer;
+
+	return (nodepointer);
+}
