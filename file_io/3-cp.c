9c30fc4bfe10b6139b6dc1ea8ed02bf85a9b697a
Gionas361 <giovannicarmona57@gmail.com>
2023-08-09 09:48:35 -0400

    Excersice 2 done and sumbmited

---
+++ file_io/3-cp.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * create_file - free's the alocated space of the list.
+ *
+ * @filename: the name of the file provided.
+ * @text_content: the letters that the text has.
+ *
+ * Return: pointer to node.
+ */
