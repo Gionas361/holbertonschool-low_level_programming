ffa6448ee823bd90c3fcc1de0dd113f2b42e0c43
Gionas361 <giovannicarmona57@gmail.com>
2023-08-09 10:17:33 -0400

    almost deleted the whole code by mistake lmao

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,10 +1,99 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *handle_char(char *file);
+void close_file(int fd);
 
 /**
- * create_file - free's the alocated space of the list.
- *
- * @filename: the name of the file provided.
- * @text_content: the letters that the text has.
- *
- * Return: pointer to node.
- */
+  * handle_char - gives 1024 bytes to manage letters.
+  * @file: The name of the char handler file that's storing chars.
+  *
+  * Return: A pointer to the char handler.
+  */
+char *handle_char(char *file)
+{
+	char *letter;
+
+	letter = malloc(sizeof(char) * 1024);
+
+	if (letter == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (letter);
+}
+
+/**
+  * file_closer - Closes files.
+  * @file: The file closed.
+  */
+void close_file(int FDfile)
+{
+	int CL;
+
+	CL = close(FDfile);
+
+	if (CL == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", FDfile);
+		exit(100);
+	}
+}
+
+/**
+  * main - Copies the text of one file to another.
+  * @argc: Things the function will copy.
+  * @argv: array for the things.
+  *
+  * Return: 0 on success.
+  * ------- If count is wrong - 97.
+  * ------- If the file to copy from doesnt exist/cannot be read - return(98).
+  * ------- If the file to copy to cannot be created/written to - return(99).
+  * ------- If file to copy to/from cannot be closed - return(100).
+  */
+int main(int argc, char *argv[])
+{
+	int FROM, TO, RD, WR;
+	char *letter;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
+	letter = handle_char(argv[2]);
+	FROM = open(argv[1], O_RDONLY);
+	RD = read(FROM, letter, 1024);
+	TO = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	while (RD > 0)
+	{
+		if (FROM == -1 || RD == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(letter);
+			exit(98);
+		}
+
+		WR = write(TO, letter, RD);
+		if (TO == -1 || WR == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(letter);
+			exit(99);
+		}
+
+		RD = read(FROM, letter, 1024);
+		TO = open(argv[2], O_WRONLY | O_APPEND);
+	}
+
+	free(letter);
+	close_file(FROM);
+	close_file(TO);
+
+	return (0);
+}
